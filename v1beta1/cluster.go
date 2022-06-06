1eae66911c32448eb35e408ca99f82735584ec4b
中弈 <zhongyi.fht@alibaba-inc.com>
2022-06-06 14:09:43 +0800

    add clusterfile reference

---
+++ v1beta1/cluster.go
@@ -0,0 +1,107 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
+)
+
+// +kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Cluster is the Schema for the InfraMetadata API
+type Cluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterSpec   `json:"spec,omitempty"`
+	Status ClusterStatus `json:"status,omitempty"`
+}
+
+func (c *Cluster) String() string {
+	data, _ := yaml.Marshal(c)
+	return string(data)
+}
+
+type RegistryConfig struct {
+	IP       string `json:"ip,omitempty"`
+	Domain   string `json:"domain,omitempty"`
+	Port     string `json:"port,omitempty"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Data     string `json:"data,omitempty"`
+}
+type ImageType string
+
+const (
+	AppImage    ImageType = "application"
+	RootfsImage ImageType = "rootfs"
+	AddonsImage ImageType = "addons"
+)
+
+type MountImage struct {
+	Name       string            `json:"name"`
+	Type       ImageType         `json:"type"`
+	ImageName  string            `json:"imageName"`
+	MountPoint string            `json:"mountPoint"`
+	Env        map[string]string `json:"env,omitempty"`
+	Labels     map[string]string `json:"labels,omitempty"`
+	Cmd        []string          `json:"cmd,omitempty"`
+}
+type ClusterStatus struct {
+	Mounts []MountImage `json:"mounts"`
+}
+
+type SSH struct {
+	User     string `json:"user,omitempty"`
+	Passwd   string `json:"passwd,omitempty"`
+	Pk       string `json:"pk,omitempty"`
+	PkPasswd string `json:"pkPasswd,omitempty"`
+	Port     uint16 `json:"port,omitempty"`
+}
+
+type Host struct {
+	IPS   []string `json:"ips,omitempty"`
+	Roles []string `json:"roles,omitempty"`
+	//overwrite env
+	Env []string `json:"env,omitempty"`
+}
+
+type ImageList []string
+
+// ClusterSpec defines the desired state of InfraMetadata
+type ClusterSpec struct {
+	// desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// Foo is an example field of Cluster. Edit Cluster_types.go to remove/update
+	Image ImageList `json:"image,omitempty"`
+	SSH   SSH       `json:"ssh"`
+	Hosts []Host    `json:"hosts,omitempty"`
+	// Why env not using map[string]string
+	// Because some argument is list, like: CertSANS=127.0.0.1 CertSANS=localhost, if ENV is map, will merge those two values
+	// but user want to InfraMetadata a list, using array we can convert it to {CertSANS:[127.0.0.1, localhost]}
+	Env []string `json:"env,omitempty"`
+	// Entrypoint array. Not executed within a shell.
+	// The docker image's ENTRYPOINT is used if this is not provided.
+	// Variable references $(VAR_NAME) are expanded using the container's environment. If a variable
+	// cannot be resolved, the reference in the input string will be unchanged. Double $$ are reduced
+	// to a single $, which allows for escaping the $(VAR_NAME) syntax: i.e. "$$(VAR_NAME)" will
+	// produce the string literal "$(VAR_NAME)". Escaped references will never be expanded, regardless
+	// of whether the variable exists or not. Cannot be updated.
+	// More info: https://kubernetes.io/docs/tasks/inject-data-application/define-command-argument-container/#running-a-command-in-a-shell
+	// +optional
+	Command []string `json:"command,omitempty"`
+}
---
+++ v1beta1/cluster_args.go
@@ -0,0 +1,204 @@
+/*
+Copyright 2022 cuisongliu@qq.com.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"fmt"
+
+	"github.com/labring/sealos/pkg/utils/maps"
+
+	"github.com/labring/sealos/pkg/utils/iputils"
+)
+
+func (c *Cluster) GetSSH() SSH {
+	return c.Spec.SSH
+}
+func (c *Cluster) SetSSH(ssh SSH) {
+	c.Spec.SSH = ssh
+}
+func (c *Cluster) GetHosts() []Host {
+	return c.Spec.Hosts
+}
+func (c *Cluster) SetHosts(hosts []Host) {
+	c.Spec.Hosts = hosts
+}
+
+func (c *Cluster) GetMasterIPList() []string {
+	return iputils.GetHostIPs(c.GetIPSByRole(MASTER))
+}
+func (c *Cluster) GetMasterIPAndPortList() []string {
+	return c.GetIPSByRole(MASTER)
+}
+
+func (c *Cluster) GetNodeIPList() []string {
+	return iputils.GetHostIPs(c.GetIPSByRole(NODE))
+}
+
+func (c *Cluster) GetNodeIPAndPortList() []string {
+	return c.GetIPSByRole(NODE)
+}
+
+func (c *Cluster) GetMaster0IP() string {
+	if len(c.Spec.Hosts) == 0 {
+		return ""
+	}
+	if len(c.Spec.Hosts[0].IPS) == 0 {
+		return ""
+	}
+	return iputils.GetHostIP(c.Spec.Hosts[0].IPS[0])
+}
+
+func (c *Cluster) GetMaster0IPAndPort() string {
+	if len(c.Spec.Hosts) == 0 {
+		return ""
+	}
+	if len(c.Spec.Hosts[0].IPS) == 0 {
+		return ""
+	}
+	return c.Spec.Hosts[0].IPS[0]
+}
+
+func (c *Cluster) GetMaster0IPAPIServer() string {
+	master0 := c.GetMaster0IP()
+	return fmt.Sprintf("https://%s:6443", master0)
+}
+
+func (c *Cluster) GetIPSByRole(role string) []string {
+	var hosts []string
+	for _, host := range c.Spec.Hosts {
+		for _, hostRole := range host.Roles {
+			if role == hostRole {
+				hosts = append(hosts, host.IPS...)
+				continue
+			}
+		}
+	}
+	return hosts
+}
+
+func (c *Cluster) GetRootfsImage(defaultMount string) *MountImage {
+	var image *MountImage
+	if c.Status.Mounts != nil {
+		for _, img := range c.Status.Mounts {
+			if img.Type == RootfsImage {
+				image = &img
+				break
+			}
+		}
+	}
+	if image == nil {
+		image = &MountImage{
+			Name:       fmt.Sprintf("%s-%d", c.Name, 0),
+			Type:       RootfsImage,
+			ImageName:  c.Spec.Image[0],
+			MountPoint: defaultMount,
+		}
+	}
+	return image
+}
+
+func (c *Cluster) FindImage(targetImage string) *MountImage {
+	var image *MountImage
+	if c.Status.Mounts != nil {
+		for _, img := range c.Status.Mounts {
+			if img.ImageName == targetImage {
+				image = &img
+				break
+			}
+		}
+	}
+	return image
+}
+func (c *Cluster) SetMountImage(targetMount *MountImage) {
+	tgMount := targetMount.DeepCopy()
+	if c.Status.Mounts != nil {
+		if tgMount != nil {
+			hasMount := false
+			for i, img := range c.Status.Mounts {
+				if img.Name == tgMount.Name && img.Type == tgMount.Type {
+					c.Status.Mounts[i] = *tgMount
+					hasMount = true
+					break
+				}
+			}
+			if !hasMount {
+				c.Status.Mounts = append(c.Status.Mounts, *tgMount)
+			}
+		}
+	}
+}
+
+func (c *Cluster) GetImageLabels() map[string]string {
+	var imageLabelMap map[string]string
+	for _, img := range c.Status.Mounts {
+		imageLabelMap = maps.MergeMap(imageLabelMap, img.Labels)
+	}
+	return imageLabelMap
+}
+
+func (c *Cluster) GetImageEnvs() map[string]string {
+	var imageEnvMap map[string]string
+	for _, img := range c.Status.Mounts {
+		imageEnvMap = maps.MergeMap(imageEnvMap, img.Env)
+	}
+	return imageEnvMap
+}
+
+func (c *Cluster) GetAppImage(defaultImageName, defaultMount string) *MountImage {
+	var image *MountImage
+	if c.Status.Mounts != nil {
+		for _, img := range c.Status.Mounts {
+			if img.Type == AppImage && img.ImageName == defaultImageName {
+				image = &img
+				break
+			}
+		}
+	}
+	if image == nil {
+		for i, img := range c.Spec.Image {
+			if img == defaultImageName {
+				image = &MountImage{
+					Name:       fmt.Sprintf("%s-%d", c.Name, i),
+					Type:       AppImage,
+					ImageName:  defaultImageName,
+					MountPoint: defaultMount,
+				}
+			}
+		}
+	}
+	return image
+}
+func (c *Cluster) HasAppImage() bool {
+	if c.Status.Mounts != nil {
+		for _, img := range c.Status.Mounts {
+			if img.Type == AppImage {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (c *Cluster) GetRolesByIP(ip string) []string {
+	var routes []string
+	for _, host := range c.Spec.Hosts {
+		if In(ip, host.IPS) {
+			return host.Roles
+		}
+	}
+	return routes
+}
